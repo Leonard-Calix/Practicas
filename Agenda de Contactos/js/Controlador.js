6fa0e9f48d207429d68419baabd4b91c25831d3f
Leonardo-Calix <bcalixvelasquez@gmail.com>
2019-08-18 12:32:12 -0600

    subir

--- Agenda de Contactos/js/Controlador.js
+++ Agenda de Contactos/js/Controlador.js
@@ -1,11 +1,4 @@
-var comentarios = [
-	{"codigo" : 1 , "nombre" : "Leonard Calix", "comentario" : "Es rica el agua"},
-	{"codigo" : 2 , "nombre" : "Cathy Valdez", "comentario" : "No mame, muy helada"},
-	{"codigo" : 1 , "nombre" : "Leonard Calix", "comentario" : "jajjajaja barbara"},
-	{"codigo" : 2 , "nombre" : "Cathy Valdez", "comentario" : "Neta, me muero"},
-	{"codigo" : 1 , "nombre" : "Cathy Valdez", "comentario" : "Neta"}
 
-	];
 
 $(document).ready(function(){
 
@@ -24,30 +17,6 @@ $(document).ready(function(){
 		}
 	});
 
-for (var i = 0; i < comentarios.length; i++) {
-
-	$("#div-comentarios").append(`
-          				<div class="row">
-							<div class="col-md-1 col-1 mr-1">
-								<span><img class="rounded-circle foto-perfil mr-2" src="img/${comentarios[i].codigo}.jpg"></span>
-							</div>
-							<div class="col-md-10  col-10">
-								<div class="p-2 mb-2 color-comentario" >
-									<small class="text-muted">
-										<span class="text-primary"><b>${comentarios[i].nombre}</b></span> 
-										${comentarios[i].comentario}
-									</small>			
-								</div>
-							</div>
-						</div>`);
-}
-
-	
-
-
-});
-
-
 
 $("#btn-agregar").click(function() {
 	
@@ -62,10 +31,10 @@ $("#btn-agregar").click(function() {
 		data: parametros,
 		success:function(respuesta){
 			console.log(respuesta);
-			if (respuesta.codigo == 1) {
+			if (respuesta.codigo==1) {
 				$("#res").append(
-					`<div class="alert alert-primary" role="alert">
-						<b>Nombre :</b> ${$("#nombre").val() }<br> <b>Numero :</b> ${$("#numero").val()}
+				`<div class="alert alert-primary" role="alert">
+					<b>Nombre :</b> ${$("#nombre").val() }<br> <b>Numero :</b> ${$("#numero").val()}
 				</div>`);
 				$("#nombre").val("");
 				$("#numero").val("");
@@ -76,32 +45,3 @@ $("#btn-agregar").click(function() {
 	});
 });
 
-
-
-$("#btn-comentar").click(function () {
-
-	var usuario = "Cathy Valdes";
-	var comentario = $("#comentario").val();
-
-	if (comentario!="") {
-		$("#div-comentarios").append(`
-          				<div class="row">
-							<div class="col-md-1 col-1 mr-1">
-								<span><img class="rounded-circle foto-perfil mr-2" src="img/p-2.jpg"></span>
-							</div>
-							<div class="col-md-10  col-10">
-								<div class="p-2 mb-2 color-comentario" >
-									<small class="text-muted">
-										<span class="text-primary"><b>${usuario}</b></span> 
-										${comentario}
-									</small>			
-								</div>
-							</div>
-						</div>`);
-	}else{
-		alert("Vacio");
-	}
-	
-	
-
-});
